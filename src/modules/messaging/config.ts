2cfe1f255119c1b5cbc98f58c68652af19602497
bhaagyaprakashh-del <bhaagyaprakashh@gmail.com>
2025-09-27 02:18:30 +0530

    Updated package-lock.json

--- src/modules/messaging/config.ts
+++
@@ -1,9 +0,0 @@
-export const TAWK = {
-  propertyId: "YOUR_TAWK_PROPERTY_ID",
-  widgetId: "YOUR_WIDGET_ID"
-};
-
-export const STORAGE_KEYS = {
-  TAWK_CONFIG: "msg_tawk_cfg",
-  BROADCASTS: "msg_broadcasts_v1"
-};
